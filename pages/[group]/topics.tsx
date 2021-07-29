057ccd9c3f1706ecef230765d48a4cebec11955f
Andy Wang <andywang0121@DESKTOP-A7F0284.localdomain>
2021-07-29 10:44:03 -0400

    modify components and remove index.html

--- pages/[group]/topics.tsx
+++ pages/[group]/topics.tsx
@@ -1,31 +1,58 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState, MouseEvent } from 'react';
 import NavBar from '../../components/NavBar';
 import Router, { useRouter } from 'next/router';
+import TopicsContainer from '../../containers/TopicsContainer';
 
 const topicsPage: FC = () => {
   const router = useRouter();
   const { group } = router.query;
-  const [user, setUser] = useState('');
-  const [topics, setTopics] = useState<string[]>([]);
+  console.log('extracted group from router.query ', group);
+  const [user, setUser] = useState('hello');
+  const [topics, setTopics] = useState<string[]>([
+    'Jobs',
+    'Careers',
+    'Events',
+    'News',
+    'Resources',
+    'Sites',
+    'Videos',
+    'React',
+    'React Native',
+    'GraphQL',
+    'Node',
+    'JavaScript',
+    'React Native',
+    'React',
+  ]);
 
-  useEffect(() => {
-    if (!group) return;
-    fetch(`http://localhost:4000/api/getTopics/${group}`)
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.user) {
-          Router.push('/');
-          return;
-        }
-        setUser(data.user);
-      })
-      .catch();
-  }, [group]);
+  // useEffect(() => {
+  //   if (!group) return;
+  //   fetch(`http://localhost:4000/api/getTopics/${group}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (!data.user || !data.topics) {
+  //         Router.push('/');
+  //         return;
+  //       }
+  //       setTopics(data.topics);
+  //       setUser(data.user);
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //       Router.push('/');
+  //     });
+  // }, [group]);
 
   return (
     <>
-      {/* <NavBar user={user} /> */}
-      <h1>{group}</h1>
+      {user ? (
+        <>
+          <NavBar username={user} />
+          <TopicsContainer topics={topics} group={group} />
+        </>
+      ) : (
+        <h1>Loading page</h1>
+      )}
     </>
   );
 };
